2bcde856782171637afb87e9df4fee3f79850745
Matheus Diniz <dfmatheusdf@gmail.com>
2022-11-27 12:08:06 -0300

    dale

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import cv2
 import numpy
-from PIL import ImageGrab
+import time
+from PIL import ImageGrab # for realtime screenshots
 from matplotlib import pyplot as plt
 
-while(True):
-    printscreen_pil = ImageGrab.grab()
-    printscreen_numpy = numpy.array(printscreen_pil.getdata(), dtype=uint8).reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
-    cv2.imshow('window', printscreen_numpy)
+start_time = time.time()
 
+FLANN_INDEX_KDTREE = 1
 MIN_MATCH_COUNT = 10
 
 img1 = cv2.imread('default_mob_skins/minecraft_zombie_head.png',0) # queryImage
-img2 = cv2.imread('mario_train.jpeg',0) # trainImage
+img1 = cv2.resize(img1, (img1.shape[0] * 30, img1.shape[1] * 30), interpolation= cv2.INTER_NEAREST)
+
+game_img = cv2.imread('zombie_ingame.jpg',0) # trainImage
+cv2.waitKey()
 
 # Initiate SIFT detector
 sift = cv2.SIFT_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp2, des2 = sift.detectAndCompute(game_img,None)
 
-kps_img = cv2.drawKeypoints(
-    image     = img2,
-    keypoints = kp2,
-    outImage  = img2
-)
-
-# matching algorythm
-FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 50)
 
@@ -52,19 +46,20 @@ if len(good)>MIN_MATCH_COUNT:
     pts = numpy.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
     dst = cv2.perspectiveTransform(pts,M) # 4 coordinates
 
-    img2 = cv2.polylines(img2,[numpy.int32(dst)],True,255,3, cv2.LINE_AA)
+    game_img = cv2.polylines(game_img,[numpy.int32(dst)],True,255,3, cv2.LINE_AA)
 
 else:
     print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
     matchesMask = None
 
 draw_params = dict(
-                   matchColor       = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask      = matchesMask, # draw only inliers
-                   flags            = 2
-                   )
+                matchColor       = (0,255,0), # draw matches in green color
+                singlePointColor = None,
+                matchesMask      = matchesMask, # draw only inliers
+                flags            = 2
+                )
 
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+img3 = cv2.drawMatches(img1,kp1,game_img,kp2,good,None,**draw_params)
+
 
 plt.imshow(img3, 'gray'),plt.show()
