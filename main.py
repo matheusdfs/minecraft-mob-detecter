e3595263ae128c84e7765af5998c6cc790188a8a
Adriano J. Paulichi <adrianopaulichi@gmail.com>
2022-12-11 01:31:01 -0300

    added manual brute force matching and some real scenarios

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from typing import List, Tuple
 import cv2
-import numpy
+from cv2 import DMatch
+import numpy as np
 import time
 
 from queryImage import QueryImage
 from matplotlib import pyplot as plt
 
-MIN_MATCH_COUNT = 10
-FLANN_INDEX_KDTREE = 1
-MAX_DISTANCE = 150
+DESC_MATCH_MIN_RATIO = 0.6
+MASK_RGB_MAX_DIST = 150
+
 
 def create_mask(queryImage, trainImage):
 
@@ -17,16 +19,14 @@ def create_mask(queryImage, trainImage):
 
     for row in queryImage:
         for element in row:
-           b, g, r = element
+            b, g, r = element
 
-           mediaB += b
-           mediaG += g
-           mediaR += r
+            mediaB += b
+            mediaG += g
+            mediaR += r
 
-    mediaB = mediaB/queryImage.size
-    mediaG = mediaG/queryImage.size
-    mediaR = mediaR/queryImage.size
-            
+    media = np.array((mediaB, mediaG, mediaR)) / queryImage.size
+
     heigth = trainImage.shape[0]
     width = trainImage.shape[1]
 
@@ -34,28 +34,52 @@ def create_mask(queryImage, trainImage):
 
     for y in range(0, heigth):
         for x in range(0, width):
-            b, g, r = trainImage[y, x]
-            dist = numpy.sqrt((mediaB - b)**2 + (mediaG - g)**2 + (mediaR - r)**2)
-            
-            if dist < MAX_DISTANCE:
+            dist = np.linalg.norm(trainImage[y, x] - media)
+
+            if dist < MASK_RGB_MAX_DIST:
                 mask[y, x] = [1, 1, 1]
             else:
                 mask[y, x] = [0, 0, 0]
-    
-    return mask[:,:,0]
+
+    return mask[:, :, 0]
+
+
+def match_descriptors(queryDescs, trainDescs) -> List[Tuple[DMatch]]:
+    matches = []
+    for queryIdx, queryDesc in enumerate(queryDescs):
+        best_dist_1 = 100000000
+        best_dist_2 = 100000000
+        train_idx_1 = 0
+
+        for train_idx, trainDesc in enumerate(trainDescs):
+            distance = np.linalg.norm(queryDesc - trainDesc)
+
+            if distance < best_dist_1:
+                best_dist_2 = best_dist_1
+
+                best_dist_1 = distance
+                train_idx_1 = train_idx
+
+            elif distance < best_dist_2:
+                best_dist_2 = distance
+
+        if best_dist_1 < best_dist_2 * DESC_MATCH_MIN_RATIO:
+            matches.append([DMatch(queryIdx, train_idx_1, best_dist_1)])
+    return matches
 
 
 def main():
     startTime = time.time()
 
     # Create object for queryImage
-    queryImage = QueryImage(cv2.imread('default_mob_skins/minecraft_zombie_head.png', cv2.IMREAD_COLOR))
+    queryImage = QueryImage(cv2.imread(
+        'default_mob_skins/minecraft_zombie_head.png', cv2.IMREAD_COLOR))
 
     # Open the ingame image
-    trainImage = cv2.imread('scenarios/zombie_ingame.jpg', cv2.IMREAD_COLOR)
+    trainImage = cv2.imread('scenarios/zmb_skt_1.png', cv2.IMREAD_COLOR)
 
     # Initiate SIFT detector
-    sift = cv2.SIFT_create(contrastThreshold= 0.02, edgeThreshold= 17)
+    sift = cv2.SIFT_create(contrastThreshold=0.02, edgeThreshold=17)
 
     # Generate the mask for the image
     mask = create_mask(queryImage.getOriginalImage(), trainImage)
@@ -64,56 +88,18 @@ def main():
     kp1, des1 = sift.detectAndCompute(queryImage.getImage(), mask)
     kp2, des2 = sift.detectAndCompute(trainImage, mask)
 
-    img = queryImage.getImage().copy()
+    # Find descriptors matches
+    matches = match_descriptors(des1, des2)
+    print('Good matches', len(matches))
 
-    img = cv2.drawKeypoints(queryImage.getImage() ,
-                    kp1 ,
-                    img ,
-                    flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow('img', img)
+    match_img = cv2.drawMatchesKnn(queryImage.getImage(), kp1, trainImage, kp2,
+                                   matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
+    print('Elapsed time:', time.time() - startTime, '(s)')
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    cv2.imshow('match_img', match_img)
+    cv2.waitKey()
 
-    matches = flann.knnMatch(des1,des2,k=2) #(tuple with matches)
-
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append(m)
-
-    if len(good)>MIN_MATCH_COUNT:
-        src_pts = numpy.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = numpy.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-        matchesMask = mask.ravel().tolist()
-
-        h,w = queryImage.getImage().shape
-        pts = numpy.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M) # 4 coordinates
-
-        trainImage = cv2.polylines(trainImage,[numpy.int32(dst)],True,255,3, cv2.LINE_AA)
-
-    else:
-        print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-        matchesMask = None
-
-    draw_params = dict(
-                    matchColor       = (0,255,0), # draw matches in green color
-                    singlePointColor = None,
-                    matchesMask      = matchesMask, # draw only inliers
-                    flags            = 2
-                    )
-
-    img3 = cv2.drawMatches(queryImage.getImage(),kp1,trainImage,kp2,good,None,**draw_params)
-
-    print(time.time() - startTime)
-
-    plt.imshow(img3, 'gray'),plt.show()
 
 if __name__ == '__main__':
     main()
--- queryImage.py
+++ queryImage.py
@@ -1,20 +1,23 @@
 import cv2
-import numpy
+import numpy as np
 import random
 
 SCALE_MULTIPLIER = 30
+
 
 class QueryImage:
     __original = None
     __imageResized = None
 
     def __init__(self, image):
-        self.__original = image.astype(numpy.float32) / 255
-        imageResized = cv2.resize(image, (image.shape[0] * 30, image.shape[1] * 30), interpolation=cv2.INTER_NEAREST)
+        self.__original = image.astype(np.float32) / 255
+        imageResized = cv2.resize(
+            image, (image.shape[0] * SCALE_MULTIPLIER, image.shape[1] * SCALE_MULTIPLIER), interpolation=cv2.INTER_NEAREST)
 
-        cv2.imshow('imageResized', imageResized)
+        cv2.imshow('queryResized', imageResized)
 
-        self.__imageResized = numpy.zeros([imageResized.shape[0] * 2, imageResized.shape[1] * 2, 3], dtype=numpy.uint8)
+        self.__imageResized = np.zeros(
+            [imageResized.shape[0] * 2, imageResized.shape[1] * 2, 3], dtype=np.uint8)
 
         h = self.__imageResized.shape[0]
         w = self.__imageResized.shape[1]
@@ -22,14 +25,14 @@ class QueryImage:
         for y in range(0, h):
             for x in range(0, w):
                 if (1/4) * h < y < (3 / 4) * h and (1 / 4) * w < x < (3 / 4) * w:
-                    self.__imageResized[y][x] = imageResized[int(numpy.floor(y - (1/4) * h))][int(numpy.floor(x - (1/4) * w))]
+                    self.__imageResized[y][x] = imageResized[int(
+                        np.floor(y - (1/4) * h))][int(np.floor(x - (1/4) * w))]
                 else:
-                    self.__imageResized[y][x] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-
+                    self.__imageResized[y][x] = [random.randint(
+                        0, 255), random.randint(0, 255), random.randint(0, 255)]
 
     def getOriginalImage(self):
         return self.__original
 
     def getImage(self):
         return self.__imageResized
-
